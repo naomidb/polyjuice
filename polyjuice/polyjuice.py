4712ecea9e4c335722a6a1abeb8a88bee4a7aa40
AjanthaRamineni <ajantha.5779@gmail.com>
2017-06-02 15:24:24 -0400

    Integrating with Docopt

--- polyjuice/polyjuice.py
+++ polyjuice/polyjuice.py
@@ -1,3 +1,21 @@
+
+docstr = """
+Polyjuice
+
+Usage: polyjuice.py [-hz] (<input_path> <output_path>) [-z --zip]
+
+Options:
+  -h --help                                     show this message and exit
+  -z --zip                                      Archives the output folder
+Instructions:
+    Run polyjuice on the ISO file or on the Extracted DICOM folder. This will give an ouput folder
+containing dicom files with unneccessary tags removed
+
+$ polyjuice.py path_to_ISOfile.iso path_to_OutputFolder
+
+Inorder to ZIP your Cleaned Output Directory
+$ polyjuice.py -z path_to_ISOfile.iso path_to_OutputFolder
+"""
 import os
 import os.path
 import dicom
@@ -5,6 +23,9 @@ import shutil
 from dicom.errors import InvalidDicomError
 import yaml
 from docopt import docopt
+
+input_dir = '<input_path>'
+output_dir = '<output_path>'
 
 def raid_snapes_cupboard():
     #find what needs to be modified or deleted from config file
@@ -38,13 +59,18 @@ def brew(dataset, out, filename):
     output = os.path.join(out, filename)
     dataset.save_as(output)
 
-def main():
+def main(args):
     import sys
-    if len(sys.argv) != 3:
-        #check for proper number of args
-        print()
+    # if len(sys.argv) != 3:
+    #     #check for proper number of args
+    #     print()
+    #     sys.exit()
+    if not (args.get(input_dir) and args.get(output_dir)):
+        print("Please Enter Input and Output files ")
         sys.exit()
-    dicom_dir, out_dir = sys.argv[1:]
+    # dicom_dir, out_dir = sys.argv[1:]
+    dicom_dir = args[input_dir]
+    out_dir = args[output_dir]
     flag = False
     if(dicom_dir.endswith(".iso")):
         # Work with ISO Stuff
@@ -54,8 +80,6 @@ def main():
         in_dir = "/Volumes/DCS/DICOM"
     else:
         in_dir = os.path.join(dicom_dir, "DICOM")
-    # Check in_dir
-
     config = raid_snapes_cupboard()
     delete_me = config.get('deletions')
     modify_me = config.get('modifications')
@@ -76,12 +100,15 @@ def main():
             except Exception, e:
                 print("{} failed".format(name))
                 print (str(e))
-    # Get output through changed filename os.rename(src, dst)
     # Working on converting into ZIP folder
     shutil.make_archive(out_dir, 'zip', out_dir)
 
+    # Checking if the file is ISO
     if flag == True:
-        # Unmount
         os.system("hdiutil unmount  /Volumes/DCS")
+
+# Integrating Things with Docopt
 if __name__ == '__main__':
-    main()
+    args = docopt(docstr)
+    main(args)
+    # main()
