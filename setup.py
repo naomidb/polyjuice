445994cd7cb55eae80bd119a43476683018193c0
Naomi Braun <looseymoose@Naomis-Mistress.local>
2017-06-28 10:00:21 -0400

    Add setup and update requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='polyjuice',
+    version='0.1.0',
+    description='Anonymize dicom files.',
+    url='https://github.com/ctsit/polyjuice',
+    author='Naomi Braun and Ajantha Ramineni',
+    author_email='',
+    license='',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=)
